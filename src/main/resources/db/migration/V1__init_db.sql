ad921bd8cf93089d6cd176cf77fc6857b5e1cdd0
Noah Swartz <noah.swartz@noahswartz.com>
2023-07-19 00:34:19 -0400

    Challenge_1

--- src/main/resources/db/migration/V1__init_db.sql
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -11,3 +11,32 @@ insert into User
     (id, firstName, lastName, username, password)
 values (1, 'Phil', 'Ingwell', 'PhilIngwell', 'Password123') ,
     (2, 'Anna', 'Conda', 'AnnaConda', 'Password234');
+
+CREATE TABLE Address 
+(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    address1 VARCHAR(255) NOT NULL,
+    address2 VARCHAR(255),
+    city VARCHAR(255) NOT NULL,
+    state VARCHAR(100) NOT NULL,
+    postal VARCHAR(10) NOT NULL
+);
+
+INSERT INTO Address
+    (id, address1, address2, city, state, postal)
+VALUES (1, '123 Street', '', 'Fenton', 'MI', '48430') ,
+    (2, '123 Road', '', 'Holland', 'OH', '43528');
+
+ALTER TABLE User 
+ADD COLUMN address_id INT UNIQUE;
+
+ALTER TABLE User
+ADD FOREIGN KEY (address_id) REFERENCES Address(id);
+
+UPDATE User
+SET User.address_id = 1
+WHERE User.id = 2;
+
+UPDATE User
+SET User.address_id = 2
+WHERE User.id = 1;
